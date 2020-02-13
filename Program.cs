ccdaea945527804f555be5812c7dfe7293da40ea
Maurício de Bastiani Silva <mauricio.bastiani@liveonsolutions.com>
2020-02-13 10:47:00 -0300

    Initial commit;

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace RPNCalculator
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                var result = Calc("15 7 1 1 + - / 3 * 2 1 1 + + -"); // Result = 5;
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static decimal Calc(string args)
+        {
+            var data = args.Split(' ');
+            var stack = new Stack<decimal>();
+
+            foreach (var item in data)
+            {
+                if (decimal.TryParse(item, out decimal outValue))
+                {
+                    stack.Push(outValue);
+                }
+                else
+                {
+                    switch (item)
+                    {
+                        case "+":
+                            stack.Push(stack.Pop() + stack.Pop());
+                            break;
+                        case "-":
+                            stack.Push(stack.Pop() - stack.Pop());
+                            break;
+                        case "*":
+                            stack.Push(stack.Pop() * stack.Pop());
+                            break;
+                        case "/":
+                            stack.Push(stack.Pop() / stack.Pop());
+                            break;
+                        default:
+                            throw new Exception($"Unknown operator: {item}");
+                    }
+                }
+            }
+
+            return Math.Round(stack.Pop());
+        }
+    }
+}
